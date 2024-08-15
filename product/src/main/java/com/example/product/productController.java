88a9308b74df23cbbf11524db325fe0690f16272
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-08-15 23:43:28 +0530

    Connect Database

---
+++ product/src/main/java/com/example/product/productController.java
@@ -0,0 +1,17 @@
+package com.example.product;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/api/v1")
+public class productController {
+//    @GetMapping("/allProduct"){
+//        public String getProduct(){
+//            return "All Product";
+//        }
+//    }
+}
